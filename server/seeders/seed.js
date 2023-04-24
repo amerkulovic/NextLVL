1b0133af4922a9f58dc14268528a9fab46938ea7
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-24 14:14:58 -0500

    added the query for the data

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,13 +1,15 @@
-const db = require('../config/connection');
-const { Profile } = require('../models');
-const profileSeeds = require('./profileSeeds.json');
+const db = require("../config/connection");
+const { Profile, Product } = require("../models");
+const profileSeeds = require("./profileSeeds.json");
+const productSeeds = require("./productSeeds.json");
 
-db.once('open', async () => {
+db.once("open", async () => {
   try {
     await Profile.deleteMany({});
     await Profile.create(profileSeeds);
+    await Product.create(productSeeds);
 
-    console.log('all done!');
+    console.log("all done!");
     process.exit(0);
   } catch (err) {
     throw err;
