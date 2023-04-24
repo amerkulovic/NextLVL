dcda9b5a1fd9acc8f3c8bb828263b09cd1ce0d79
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-24 12:55:08 -0500

    created the template for the trainers page

--- client/src/components/UI/TrainerProfile.js
+++ client/src/components/UI/TrainerProfile.js
@@ -1,7 +1,10 @@
+import "./trainers.css";
+
 const TrainerProfile = (props) => {
   return (
-    <div className="bg-yellow-400 my-4 relative" style={{ height: "500px", width: "420px" }}>
-      <div className="text-center flex flex-col justify-center items-center h-full">
+    <div className="mt-5 mb-40 trainer-card">
+      <img className="trainer-image" src={props.image} />
+      <div className="absolute bottom-8 title text-3xl">
         <h1>{props.name}</h1>
         <p>{props.desc}</p>
       </div>
--- client/src/pages/Trainers.js
+++ client/src/pages/Trainers.js
@@ -1,16 +1,22 @@
 import TrainerProfile from "../components/UI/TrainerProfile";
+import tanktop from "../images/black-tanktop.jpg";
+import hayley from "../images/hayley.jpg";
+import redtop from "../images/red-tanktop.jpg";
+import gordon from "../images/gordon.jpg";
+import buffguy from "../images/buff-guy.jpg";
+import julia from "../images/julia.jpg";
 
 const Trainers = () => {
   return (
     <>
       <div className="pt-40 bg-[#2C2E43]" />
       <div className="flex flex-wrap justify-around">
-        <TrainerProfile name="Jack" desc="Weightlifting" />
-        <TrainerProfile name="Jack" desc="Weightlifting" />
-        <TrainerProfile name="Jack" desc="Weightlifting" />
-        <TrainerProfile name="Jack" desc="Weightlifting" />
-        <TrainerProfile name="Jack" desc="Weightlifting" />
-        <TrainerProfile name="Jack" desc="Weightlifting" />
+        <TrainerProfile name="Jack" desc="Weightlifting" image={tanktop} />
+        <TrainerProfile name="Hayley" desc="Crossfit" image={hayley} />
+        <TrainerProfile name="Dominic" desc="Powerlifting" image={buffguy} />
+        <TrainerProfile name="Julia" desc="Yoga" image={julia} />
+        <TrainerProfile name="Ranjeet" desc="Calisthenics" image={redtop} />
+        <TrainerProfile name="Gordon" desc="Crossfit" image={gordon} />
       </div>
     </>
   );
