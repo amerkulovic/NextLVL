35181e1a5af520b4055009599b4f984d6373c3c2
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-23 19:24:51 -0500

    edited styling

--- client/src/components/UI/Quote.js
+++ client/src/components/UI/Quote.js
@@ -1,7 +1,7 @@
 const Quote = (props) => {
   return (
     <>
-      <section className="bg-[#2C2E43] text-[#FFD523] text-3xl flex justify-center py-14">
+      <section className="text-3xl flex justify-center py-14">
         <div className="flex flex-col items-center m-5">
           <h1 className="font-extralight pb-4">"{props.text}"</h1>
           <h2 className="font-bold" style={{ fontFamily: "Bruno Ace SC" }}>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
       {/* bg-gradient-to-r from-[#2C2E43] via-[#595260] to-[#595260] */}
       <img style={{ height: "700px", width: "100%" }} src={homeLogo} />
       <Quote text="It is a shame for a man to grow old without seeing the beauty and strength of which his body is capable." author="Socrates" />
-      <section className="flex flex-row flex-wrap justify-around bg-gradient-to-b from-[#2C2E43] from-0% via-[#595260] via-1%  to-[#2C2E43] to-99% py-10">
+      <section className="flex flex-row flex-wrap justify-around bg-gradient-to-b from-white from-0% via-[#adafc5] via-40%  to-[#2C2E43] to-70% py-10">
         <CircleText image={weights} text="Pump Iron and Get Stronger!" />
         <CircleText image={stretching} text="Lose Weight and Feel Good!" />
         <CircleText image={lifting} text="Become The Best Version of Yourself!" />
