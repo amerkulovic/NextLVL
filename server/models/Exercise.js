548eb99f16380954113f9f9f5b02c411ffd922b0
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-26 14:02:41 -0500

    created Exercise model

---
+++ server/models/Exercise.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const exerciseSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  muscle: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  equipment: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  imageURL: {
+    type: String,
+    required: true,
+  },
+  difficulty: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+});
+
+const Exercise = model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
