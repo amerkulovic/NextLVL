e5a19440fff8760a13034adc6f3891a21a81ed15
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-27 13:02:10 -0500

    added filter button and functionality

--- client/src/pages/Exercise.js
+++ client/src/pages/Exercise.js
@@ -1,18 +1,70 @@
 import { QUERY_EXERCISES } from "../utils/queries";
 import { useQuery } from "@apollo/client";
 import ExerciseItem from "../components/UI/ExerciseItem";
+import { useState } from "react";
 
 const Exercise = () => {
   const { loading, data } = useQuery(QUERY_EXERCISES);
 
   const exercises = data?.exercises || [];
-  console.log(exercises);
+  let [filteredExercises, setFilteredExercises] = useState([...exercises]);
+
+  const filterExercises = (event) => {
+    const value = event.target.value;
+
+    setFilteredExercises((filteredExercises = exercises.filter((exercise) => exercise.muscle === value)));
+  };
+
   return (
     <>
-      <div className="pt-40 bg-[#2C2E43]" />
+      <div className="pt-40 bg-[#2C2E43] flex justify-end">
+        <div className="flex flex-col text-center items-center mr-11">
+          <button className="bg-yellow-400 py-3 px-10" style={{ fontFamily: "Bruno Ace SC" }}>
+            Filter
+          </button>
+          <section className="bg-white px-6 flex flex-col font-light">
+            <button className="pt-0.5" onClick={() => setFilteredExercises([...exercises])}>
+              All
+            </button>
+            <button value="biceps" className="py-0.5" onClick={filterExercises}>
+              Biceps
+            </button>
+            <button value="triceps" className="py-0.5" onClick={filterExercises}>
+              Triceps
+            </button>
+            <button value="traps" className="py-0.5" onClick={filterExercises}>
+              Traps
+            </button>
+            <button value="quadriceps" className="py-0.5" onClick={filterExercises}>
+              Quadriceps
+            </button>
+            <button value="hamstrings" className="py-0.5" onClick={filterExercises}>
+              Hamstrings
+            </button>
+            <button value="glutes" className="py-0.5" onClick={filterExercises}>
+              Glutes
+            </button>
+            <button value="calves" className="py-0.5" onClick={filterExercises}>
+              Calves
+            </button>
+            <button value="chest" className="py-0.5" onClick={filterExercises}>
+              Chest
+            </button>
+            <button value="lats" className="py-0.5" onClick={filterExercises}>
+              Lats
+            </button>
+            <button value="lower_back" className="py-0.5" onClick={filterExercises}>
+              Lower Back
+            </button>
+            <button value="middle_back" className="py-0.5" onClick={filterExercises}>
+              Middle Back
+            </button>
+          </section>
+        </div>
+      </div>
       <section className="bg-gradient-to-b from-[#2C2E43] from-7% via-white via-50% to-[#2C2E43] to-98% flex flex-wrap justify-around">
-        {exercises.map((exercise) => (
-          <ExerciseItem title={exercise.title} muscle={exercise.muscle} description={exercise.instructions} image={exercise.imageURL} />
+        {filteredExercises.map((exercise) => (
+          <ExerciseItem key={exercise.title} title={exercise.title} muscle={exercise.muscle} description={exercise.instructions} image={exercise.imageURL} />
         ))}
       </section>
     </>
