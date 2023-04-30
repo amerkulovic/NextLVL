5754e1a1615c2cc2d12a1800471e7d1da5990044
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-29 23:17:24 -0500

    made footer mobile responsive

--- client/src/components/Footer/ContactFooter.js
+++ client/src/components/Footer/ContactFooter.js
@@ -1,6 +1,6 @@
 const ContactFooter = () => {
   return (
-    <div className="bg-[#2C2E43] border-b border-yellow-400 pt-32 pl-5 pb-10 flex justify-between items-center">
+    <div className="bg-[#2C2E43] border-b border-yellow-400 pt-32 pl-5 pb-10 flex justify-between max-sm:flex-col items-center">
       <section className="flex flex-col justify-start text-white pl-4">
         <h1 className="text-5xl pb-2" style={{ fontFamily: "Bruno Ace SC" }}>
           Stay Updated
@@ -11,7 +11,7 @@ const ContactFooter = () => {
           We respect your privacy and will never share your information with any third-party vendors.
         </p>
       </section>
-      <section className="flex flex-row mr-10 items-center">
+      <section className="flex flex-row mr-10 items-center max-sm:mt-5">
         <input style={{ fontFamily: "Bruno Ace SC" }} placeholder="email" className="h-14 px-4 mr-2" />
         <button className="bg-yellow-400 p-4 h-14" style={{ fontFamily: "Bruno Ace SC" }}>
           Sign Up
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <>
       <ContactFooter />
-      <footer className="bg-[#2C2E43] flex flex-wrap justify-between items-center pl-5 py-5">
+      <footer className="bg-[#2C2E43] flex flex-wrap justify-between max-sm:justify-center items-center pl-5 py-5">
         <section className="flex flex-row">
           <FooterSection header="Discover" item1="About" item1link="/about" item2="Trainers" item2link="/trainers" item3="Exercises" item3link="/exercises" item4="Shop" item4link="/shop" item5="Contact" item5link="/contact" />
           <FooterSection header="Contact" item1="NextLVL@gmail.com" item2="123 Level Up Street, Chicago IL" item3="(555)-555-5555" />
