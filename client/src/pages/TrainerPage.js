333fb6a1bf2c6eaaba3e779476ddae113ee6c886
Amer Kulovic <amer.kulovic@yahoo.com>
2025-09-02 14:15:27 -0500

    cleaned up code in TrainerPage component

--- client/src/pages/TrainerPage.js
+++ client/src/pages/TrainerPage.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useParams } from "react-router-dom";
 import TrainerInfo from "../components/UI/TrainerInfo";
 import tanktop from "../images/black-tanktop.jpg";
